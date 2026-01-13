d4f8a975cee3b9275aca83ac712b2aa1ca6067db
Igor Chicherin <ashzak990@gmail.com>
2026-01-13 13:38:57 +0300

    terminal toggle

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+--
+-- map("n", "<c-/>", function()
+--   Snacks.terminal(nil, { cwd = vim.uv.cwd() })
+-- end, { desc = "Terminal (Root Dir)" })
+--
+
+-- Terminal Mappings
+map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
